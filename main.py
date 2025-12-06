6e4858c1762a46d8d32c2630f1ec055b4acf2547
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-12-06 16:49:55 +0800

    Refactor logging in hello_pubsub function and update dependencies
    - Replaced print statement with logger.info for better logging in main.py.
    - Updated Poetry version in poetry.lock to 2.2.1.
    - Added jinja2 version 3.1.6 to pyproject.toml, requirements-dev.txt, and requirements.txt for template rendering support.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import functions_framework
 from cloudevents.http.event import CloudEvent
+from app.helpers.logger import get_logger
 
+logger = get_logger("hello_pubsub")
 
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event: CloudEvent) -> None:
-   print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+   logger.info(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
