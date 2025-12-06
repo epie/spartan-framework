a5c8f8e9b9e3d41e52fa10568c4f58908769133d
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-12-06 18:07:57 +0800

    Update logger initialization to use correct module name for clarity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cloudevents.http.event import CloudEvent
 from app.helpers.logger import get_logger
 
 
-logger = get_logger("spartan.lazaro.hello_pubsub")
+logger = get_logger("spartan.lazaro.main")
 
 @functions_framework.cloud_event
 def main(cloud_event: CloudEvent) -> None:
