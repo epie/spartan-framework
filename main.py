d200a4b32a9de2d201cd7f46fec36f77090decb4
Sydel Palinlin <sydel.palinlin@gmail.com>
2025-12-06 17:09:45 +0800

    Remove AppService class and related unit tests; update main function and logging configuration; add boto3 and botocore dependencies to project files

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import functions_framework
 from cloudevents.http.event import CloudEvent
 from app.helpers.logger import get_logger
 
-logger = get_logger("hello_pubsub")
+logger = get_logger("spartan.lazaro.hello_pubsub")
 
 @functions_framework.cloud_event
-def hello_pubsub(cloud_event: CloudEvent) -> None:
+def main(cloud_event: CloudEvent) -> None:
    logger.info(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
